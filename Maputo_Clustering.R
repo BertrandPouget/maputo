92cfc7b7559d8d8fe9c49c7e2f333caeca124b01
BertrandPouget <markhillsxd@gmail.com>
2022-06-29 09:43:08 +0200

    Inizio

---
+++ Maputo_Clustering.R
@@ -0,0 +1,40 @@
+library(sf)
+library(tidyverse)
+library(rgl)
+
+setwd("C:/Users/markh/Documents/RHome/AppliedStat/Project")
+
+#Creazione dataset senza geometry
+dat <- st_read("datonite.shp")
+dat <- st_drop_geometry(dat)
+dat <- dat[which(dat$osm_surf != "unk"),]
+dat <- dat[order(dat$osm_surf),]
+rownames(dat) <- 1:nrow(dat)
+categories <- dat[,3]
+
+#Creazione dataset numerico
+keeps <- c("rmean","rvar","rmax")
+dat_num <- dat[keeps]
+attach(dat)
+open3d()
+plot3d(dat_num, col = ifelse(osm_surf=="paved","red","blue"))
+pairs(dat_num)
+detach(dat)
+
+#Definizione distanza
+dat.e <- dist(dat_num, method = "euclidean")
+#x11()
+#image(1:2558,1:2558,as.matrix(dat.e), main='metrics: Euclidean', asp=1, xlab='i', ylab='j')
+
+#Definizione clusters
+dat.ec <- hclust(dat.e, method = "ward.D2")
+
+plot(dat.ec, main='euclidean-complete', hang=-0.1, xlab='', labels=F, cex=0.6, sub='')
+rect.hclust(dat.ec, k=2)
+
+cluster.ec <- cutree(dat.ec, k=2)
+cluster.ec
+
+open3d()
+plot3d(dat_num, col=cluster.ec+3, pch=19)
+table(label.true = categories, label.cluster = cluster.ec)
---
+++ Maputo_Manova.R
@@ -0,0 +1,73 @@
+library(sf)
+library(tidyverse)
+
+setwd("C:/Users/markh/Documents/RHome/AppliedStat/Project")
+load("C:/Users/markh/Documents/RHome/AppliedStat/Lab 5 - Box-Cox + Multivariate CR/mcshapiro.test.RData")
+
+#_______________________________________________________________________________
+##### One-way MANOVA
+##### (p=6, g=2)
+#####---------------
+
+dat <- st_read("datonite.shp")
+dat <- dat[which(dat$osm_surf != "unk"),]
+dat <- dat[order(dat$osm_surf),]
+rownames(dat) <- 1:nrow(dat)
+
+attach(dat)
+dat_num <- cbind.data.frame(rmean, rvar, rmed)
+detach(dat)
+
+categories <- factor(dat$osm_surf, labels=c('paved', 'unpaved'))
+
+i1 <- which(categories=='paved')
+i2 <- which(categories=='unpaved')
+
+par(mfrow=c(1,3))
+boxplot(dat_num$rm~categories, col = rainbow(2))
+boxplot(dat_num$gm~categories, col = rainbow(2))
+boxplot(dat_num$bm~categories, col = rainbow(2))
+
+n1 <- length(i1)
+n2 <- length(i2)
+n  <- n1+n2
+
+g  <- length(levels(categories))
+p  <- 2
+
+fit <- manova(as.matrix(dat_num) ~ categories)
+summary.manova(fit,test="Wilks")
+
+summary.aov(fit)
+
+### Via Bonferroni
+alpha <- 0.05
+k <- p*g*(g-1)/2
+qT <- qt(1-alpha/(2*k), n-g)
+
+W <- summary.manova(fit)$SS$Residuals
+m  <- sapply(dat_num,mean)         # estimates mu
+m1 <- sapply(dat_num[i1,],mean)    # estimates mu.1=mu+tau.1
+m2 <- sapply(dat_num[i2,],mean)    # estimates mu.2=mu+tau.2
+
+inf12 <- m1-m2 - qT * sqrt( diag(W)/(n-g) * (1/n1+1/n2) )
+sup12 <- m1-m2 + qT * sqrt( diag(W)/(n-g) * (1/n1+1/n2) )
+
+CI <- list(paved_unpaved=cbind(inf12, sup12))
+CI
+
+mg <- rbind(m1,m2)
+sp.name <- c('rmean','rvar','rmed')
+for(k in 1:3){
+  plot(c(1,g*(g-1)/2),ylim=c(-4,4), xlim=c(1,3), pch='', 
+       xlab='pairs treat', ylab=paste('CI tau',k), 
+       main=paste('CI tau',sp.name[k]))
+  lines (c(1,1), c(CI[[1]][k,1],CI[[1]][k,2])); 
+  points(1, mg[1,k]-mg[2,k], pch=16); 
+  points(1, CI[[1]][k,1], col=rainbow(g)[2], pch=16); 
+  points(1, CI[[1]][k,2], col=rainbow(g)[1], pch=16);  
+  abline(h=0)
+}
+
+dev.off()
+
