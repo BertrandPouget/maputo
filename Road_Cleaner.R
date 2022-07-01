bf8de3f6182e333abd2e01113d9fea78a572b706
Vinavil334 <84518386+Vinavil334@users.noreply.github.com>
2022-07-01 17:42:54 +0200

    Cleaned Road Cleaner
    alberiiiii
    
    Co-Authored-By: BertrandPouget <84519000+BertrandPouget@users.noreply.github.com>

--- Road_Cleaner.R
+++ Road_Cleaner.R
@@ -2,96 +2,64 @@
 #Ricorda di settare la tua working directory da Session!
 
 rm(list=ls())
+
 library(sf)
 library(sp)
 library(ggplot2)
 library(rgdal)
-library(sp)
+library(rgl)
 library(raster)
+
 data = st_read("datonite.shp")
 data = data[order(as.numeric(data$id)),]
 
-head(data[which(data$osm_surf=="paved"),])
-head(data[which(data$osm_surf=="unpaved"),])
-
 #Importiamo un esempio di immagine e plottiamola
 
-img_paved = brick("img__1573.tif")
-plotRGB(img_paved);
+img = brick("img__1573.tif") #Vai nella sua directory!
+plotRGB(img)
+names(img) = c("redBand","greenBand", "blueBand", "null")
 
-#Visualizziamo i vari colori separatamente
+#Dividiamo il dataset in due parti per motivi computazionali
 
-names(img_paved) = c("redBand","greenBand", "blueBand", "null")
-plot(img_paved, col=gray(1:100/100)) 
+dat = data.frame(values(img))
+attach(dat)
+len=dim(dat)[1]/2
+dat1 = dat[1:len,]
+dat2 = dat[-c(1:len),]
+detach(dat)
 
-#Plottiamo gli istogrammi (di tutta l'immagine)
+#Definiamo distanza e clusters e plottiamo
 
-{
-  par(mfrow=c(1,3))
-  hist(img_paved$redBand, xlab="", main='red values', col='red')
-  hist(img_paved$greenBand, xlab="", main='green values', col='green')
-  hist(img_paved$blueBand, xlab="", main='blue values', col='blue')
-  par(mfrow=c(1,1))
-}
-
-#Rimuoviamo i pixel neri e riplottiamo gli istogrammi
-
-val_paved = data.frame(values(img_paved)[which(apply(values(img_paved),1,sum)!=0),])
-{
-  par(mfrow=c(1,3))
-  hist(val_paved$redBand, xlab="", main='red values', col='red')
-  hist(val_paved$greenBand, xlab="", main='green values', col='green')
-  hist(val_paved$blueBand, xlab="", main='blue values', col='blue')
-  par(mfrow=c(1,1))
-}
-
-graphics.off()
-
-#Plottiamo i valori dei tre colori
-
-attach(val_paved)
-plot3d(redBand, greenBand, blueBand)
-detach(val_paved)
-
-#Facciamo clustering sui pixels
-
-val_tot = data.frame(values(img_paved))
-attach(val_tot)
-
-#Definiamo distanza e clusters
-
-dat.e <- dist(val_tot[,1:3], method = "euclidean")
+dat.e <- dist(dat1[,1:3], method = "euclidean")
 dat.ec <- hclust(dat.e, method = "complete")
+cluster.ec1 <- cutree(dat.ec, k=3)
 
-#Plottiamo il dendrogramma con la suddivisione tramite rettangoli,
-#calcoliamo i clusters e plottiamo
+dat.e <- dist(dat2[,1:3], method = "euclidean")
+dat.ec <- hclust(dat.e, method = "complete")
+cluster.ec2 <- cutree(dat.ec, k=3)
 
-plot(dat.ec, main='euclidean-complete', hang=-0.1, xlab='', labels=F, cex=0.6, sub='')
-rect.hclust(dat.ec, k=2)
+cluster.ec <- c(cluster.ec1,cluster.ec2)
 
-cluster.ec <- cutree(dat.ec, k=3)
-cluster.ec
-
-plot3d(val_tot[,1:3], col=cluster.ec+1, pch=19)
+plot3d(dat[,1:3], col=cluster.ec+1, pch=19)
 
 #Coloriamo i pixel delle strade a seconda del cluster d'appartenenza
 
-for(i in 1:dim(values(img_paved))[1])
-  if(sum(values(img_paved)[i,])!=0)
+for(i in 1:dim(values(img))[1])
+  if(sum(values(img)[i,])!=0)
   {
     if(cluster.ec[i]==1)
     {
-      values(img_paved)[i,1]=255
-      values(img_paved)[i,2]=0
-      values(img_paved)[i,3]=0
+      values(img)[i,1]=0
+      values(img)[i,2]=0
+      values(img)[i,3]=0
     }
-    else if(cluster.ec[i]!=2)
+    else
     {
-      values(img_paved)[i,1]=0
-      values(img_paved)[i,2]=0
-      values(img_paved)[i,3]=255
+      values(img)[i,1]=values(img)[i,1]
+      values(img)[i,2]=values(img)[i,2]
+      values(img)[i,3]=values(img)[i,3]
     }
   }
         
 x11()
-plotRGB(img_paved)
+plotRGB(img)
